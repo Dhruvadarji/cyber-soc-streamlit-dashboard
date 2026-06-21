ec5f81cbc6580030a45bd575d3dd28cdf28fe585
Dhruvadarji <140485336+Dhruvadarji@users.noreply.github.com>
2026-06-21 14:01:23 +0530

    Update device_checker.py

--- modules/device_checker.py
+++ modules/device_checker.py
@@ -1,12 +1,12 @@
 import pandas as pd
+import os
 
 def check_device(username, device):
 
+    if not os.path.exists("data/known_devices.csv"):
+        return False
+
     df = pd.read_csv("data/known_devices.csv")
 
-    match = df[
-        (df["Username"] == username) &
-        (df["Device"] == device)
-    ]
-
-    return len(match) > 0
+    return ((df["username"] == username) &
+            (df["device"] == device)).any()
